8c8e0cf5350ef0af2aaef0ff2f3dedbccc30c5ba
Dat-Cris <ddat261103@gmail.com>
2024-07-06 15:05:29 +0700

    Create final-exam-04

--- src/main/java/com/vti/service/IUserService.java
+++ src/main/java/com/vti/service/IUserService.java
@@ -2,17 +2,12 @@ package com.vti.service;
 
 import com.vti.entity.User;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public interface IUserService {
-    List<User> findAll();
+    List<User> findEmployeeByProjectId(int projectId);
 
-    User findById(int id);
+    List<User> findAllManager();
 
-    User findByEmailAndPassword(String email, String password);
-
-    int create(String fullName, String email);
-
-    int deleteById(int id);
+    User findManagerByEmailAndPassword(String email, String password);
 }
